fa4aa93582c71e4cdb9ec33ef3be76486e20f7ac
GuccGod <coprivnicool@gmail.com>
2020-08-21 16:59:29 +0000

    Added some fun art

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+ /*Gideon Coprivnicar
+AP Computer Science 
+8/21/20*/
+public class HelloWorld{
+  public static void main(String args[]){
+    System.out.print("~~~~~~~");
+    System.out.print("(ツ)_/¯");
+    System.out.print("~~~~~");
+    /*A small little man who is shrugging because he lost his small little man legs and his left arm (because it has a backslash) Also he's swimming in an ocean.*/
+  }
+}
