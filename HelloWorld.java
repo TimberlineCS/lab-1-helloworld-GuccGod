80aede11d79895e7815f7a61ecc0baf9e6147c22
GuccGod <coprivnicool@gmail.com>
2020-08-24 16:15:45 +0000

    Wrote the code

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/*Gideon Coprivnicar
+AP CompSci
+8/19/20*/
+public class HelloWorld{
+  public static void main(String args[]){
+    System.out.print("~~~~~~~~(ツ)~~~~~~~~");
+    /* It's a small little swimming man, he's a little confused because he doesn't have any legs and will probably drown soon*/
+  }
+}
